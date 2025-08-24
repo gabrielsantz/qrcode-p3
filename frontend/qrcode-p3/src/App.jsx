f3564025f202c38b5d70e8346a4363728e9c7409
Vinícius Gabriel Silva Santiago <vcgabriel@outlook.com>
2025-08-24 14:24:07 -0300

    remove supabase

--- frontend/qrcode-p3/src/App.jsx
+++ frontend/qrcode-p3/src/App.jsx
@@ -1,37 +1,11 @@
 import './index.css';
-import { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { Routes, Route } from 'react-router-dom';
 
 
 import { Home, ProfessorHomepage, QrReaderPage, StudentHomepage } from './pages';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function App() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
-  if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
-  } else {
     return (
       <Routes>
         <Route path="/" element={<Home />} />
@@ -40,5 +14,4 @@ export default function App() {
         <Route path="/student/qr-reader" element={<QrReaderPage />} />
       </Routes>
     );
-  }
 }
