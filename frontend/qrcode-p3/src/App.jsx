577c7727f04cb126d54ebb10226af5ff54c9d063
Vinícius Gabriel Silva Santiago <vcgabriel@outlook.com>
2025-08-23 23:21:22 -0300

    feat: add auth via supabase

--- frontend/qrcode-p3/src/App.jsx
+++ frontend/qrcode-p3/src/App.jsx
@@ -1,20 +1,47 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import StudentHomepage from './pages/StudentHomepage'
-import ProfessorHomepage from './pages/ProfessorHomepage'
-import QrReaderPage from './pages/QrReader'
+import './index.css';
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Routes, Route } from 'react-router-dom';
 
 
+import Home from './pages/Home';
+import StudentHomepage from './pages/StudentHomepage';
+import ProfessorHomepage from './pages/ProfessorHomepage';
+import QrReaderPage from './pages/QrReader';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/student/" element={<StudentHomepage />} />
-      <Route path="/professor/" element={<ProfessorHomepage />} />
-      <Route path="/student/qr-reader" element={<QrReaderPage />} />
+  const [session, setSession] = useState(null);
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-    </Routes>
-  )
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student/" element={<StudentHomepage />} />
+        <Route path="/professor/" element={<ProfessorHomepage />} />
+        <Route path="/student/qr-reader" element={<QrReaderPage />} />
+      </Routes>
+    );
+  }
 }
--- frontend/qrcode-p3/vite.config.js
+++ frontend/qrcode-p3/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'e6e5e1363b0a.ngrok-free.app'
     ]
   }
 })
