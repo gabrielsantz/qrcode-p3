f03c2bfcd22835d792ffad6d23e84ad4598024d2
Vinícius Gabriel Silva Santiago <vcgabriel@outlook.com>
2025-08-24 14:18:24 -0300

    refact: exportar paginas em arquivo unico

---
+++ frontend/qrcode-p3/src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import ProfessorHomepage from "./ProfessorHomepage";
+import QrReaderPage from "./QrReader";
+import StudentHomepage from "./StudentHomepage";
+
+export  {
+    Home,
+    ProfessorHomepage,
+    QrReaderPage,
+    StudentHomepage
+}
+
