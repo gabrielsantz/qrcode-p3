fec46c0ec64c3c5983e1de42344de8aff0b84991
Vinícius Gabriel Silva Santiago <vcgabriel@outlook.com>
2025-08-26 12:33:00 -0300

    refact: tornar campo matricula opcional

--- backend/app/core/models/user.py
+++ backend/app/core/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import func, text
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 from .table_registry import table_registry
 import uuid
 from datetime import datetime
@@ -16,7 +17,7 @@ class User:
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(nullable=False)
-    registration: Mapped[str] = mapped_column(unique=True, nullable=False)
+    registration: Mapped[Optional[str]] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     role: Mapped[UserRole] = mapped_column(nullable=False)
@@ -25,21 +26,25 @@ class User:
     
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
-    def __init__(self, name:str, registration:str, email: str, hashed_password: str, role: UserRole):
+    def __init__(self, name:str, email: str, hashed_password: str, role: UserRole, registration: str | None = None):
         if not name:
             raise ValueError("O nome é obrigatório.")
-        if not registration:
-            raise ValueError("A matrícula é obrigatória.")
         if not email:
             raise ValueError("O e-mail é obrigatório.")
         if not hashed_password:
             raise ValueError("A senha é obrigatória.")
         if not role:
             raise ValueError("A role é obrigatória.")
+
+        if role == UserRole.STUDENT and not registration:
+            raise ValueError("A matrícula é obrigatória para alunos.")
         
+        if role != UserRole.STUDENT and registration is not None:
+            raise ValueError("A matrícula só é permitida para alunos.")
 
         self.name = name
         self.registration = registration
         self.email = email
         self.hashed_password = hashed_password
         self.role = role
+
