df9a3d359b1fcce735af8cb98bb71ec2d1be6289
Vinícius Gabriel Silva Santiago <vcgabriel@outlook.com>
2025-08-28 10:53:10 -0300

    fix: consertar import

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,9 +7,7 @@ from sqlalchemy.orm import Session
 from app.infra.config import settings
 from app.infra.db.connection import get_db
 
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from app.core.models import User
+from app.core.models import User
 
 
 
