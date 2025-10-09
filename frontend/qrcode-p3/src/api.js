81abc8140d2caa46a79f9ccb1ada35d419f738c4
Vinícius Santiago <vcgabriel@outlook.com>
2025-10-09 14:14:37 -0300

    fix: correção de objeto

--- frontend/qrcode-p3/src/api.js
+++ frontend/qrcode-p3/src/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const apiClient = axios.create(
+const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('access_token');
