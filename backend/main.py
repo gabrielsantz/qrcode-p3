c535ada4826e1a04d4b80845d064f8f5ef39d92d
Vinícius Santiago <vcgabriel@outlook.com>
2025-10-11 17:40:59 -0300

    feat: implementar geração de pdf no front

--- backend/main.py
+++ backend/main.py
@@ -36,5 +36,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"] 
 )
 app.include_router(api_router, prefix="/v1") 
